930bd4992b24ea3fc6e261951880f0f8f67ba348
raptattention <klaw1794@gmail.com>
2015-07-14 17:58:00 +0800

    Added new formats

--- js/application5x5.js
+++ js/application5x5.js
@@ -9,9 +9,9 @@ $(document).ready(function(){
 
   var checkNoZeroes = function(){
     var hasZeroes = false;
-    for(i=1; i<=4; i++){
+    for(i=1; i<=5; i++){
       var GridRow="grid_"+i;
-      for (j=1; j<=4; j++){
+      for (j=1; j<=5; j++){
         var GridCol="column"+j;
         if ($('#'+GridRow).children('#'+GridCol).text()=='0'){
           hasZeroes=true;
@@ -22,9 +22,9 @@ $(document).ready(function(){
   }
 
   var reset = function(){
-    for(i=1; i<=4; i++){
+    for(i=1; i<=5; i++){
       var resetGridRow="grid_"+i;
-      for (j=1; j<=4; j++){
+      for (j=1; j<=5; j++){
         var resetGridCol="column"+j;
         $('#'+resetGridRow).children('#'+resetGridCol).attr('myAttr','canMove');
         $('#'+resetGridRow).children('#'+resetGridCol).attr('combine','canCombine');
@@ -33,9 +33,9 @@ $(document).ready(function(){
   }
 
   var setColor = function(){
-    for(i=1; i<=4; i++){
+    for(i=1; i<=5; i++){
       var GridRow="grid_"+i;
-      for (j=1; j<=4; j++){
+      for (j=1; j<=5; j++){
         var GridCol="column"+j;
         var elementValue=$('#'+GridRow).children('#'+GridCol);
         var elementValueText=elementValue.text()
@@ -84,6 +84,26 @@ $(document).ready(function(){
             elementValue.css('background-color','#EDC22E');
             elementValue.css('color','white');
             break;
+          case "4096":
+            elementValue.css('background-color','#FFA500');
+            elementValue.css('color','white');
+            break;
+          case "8192":
+            elementValue.css('background-color','#98F5FF');
+            elementValue.css('color','white');
+            break;
+          case "16384":
+            elementValue.css('background-color','#00C5CD');
+            elementValue.css('color','white');
+            break;
+          case "32768":
+            elementValue.css('background-color','#5CACEE');
+            elementValue.css('color','white');
+            break;
+          case "65536":
+            elementValue.css('background-color','#32CD32');
+            elementValue.css('color','white');
+            break;
           default:
             elementValue.css('background-color','#CCC0B3');
             elementValue.css('color','#CCC0B3');
@@ -95,8 +115,8 @@ $(document).ready(function(){
   var randomNumberGeneration = function(){
     var generated=false;
     while (generated==false){
-      var colNum=Math.floor(Math.random()*3.99+1);
-      var rowNum=Math.floor(Math.random()*3.99+1);
+      var colNum=Math.floor(Math.random()*4.99+1);
+      var rowNum=Math.floor(Math.random()*4.99+1);
       gridCol="column"+colNum;
       gridRow="grid_"+rowNum;
       if($('#'+gridRow).children('#'+gridCol).text()=="0"){
@@ -112,9 +132,9 @@ $(document).ready(function(){
   randomNumberGeneration();
 
   $('.btn-default').click(function(){
-    for(i=1; i<=4; i++){
+    for(i=1; i<=5; i++){
       var resetGridRow="grid_"+i;
-      for (j=1; j<=4; j++){
+      for (j=1; j<=5; j++){
         var resetGridCol="column"+j;
         $('#'+resetGridRow).children('#'+resetGridCol).text('0');
         $('#'+resetGridRow).children('#'+resetGridCol).text('0');
@@ -130,11 +150,11 @@ $(document).ready(function(){
 
   var leftMove = function(){
   	var hasMoved=false;
-  	for (i=1; i<=4; i++){
+  	for (i=1; i<=5; i++){
   		gridRow="grid_"+i;
   		$('#'+gridRow).children('#column1').attr('myAttr','cantMove');
-  		while(($('#'+gridRow).children('#column2').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column3').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column4').attr('myAttr')=='canMove')){
-  			for (j=2; j<=4; j++){
+  		while(($('#'+gridRow).children('#column2').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column3').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column4').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column5').attr('myAttr')=='canMove')){
+  			for (j=2; j<=5; j++){
   				gridCol="column"+j;
   				gridColPrev="column"+(j-1);
   				if($('#'+gridRow).children('#'+gridCol).text()!="0"){
@@ -171,11 +191,11 @@ $(document).ready(function(){
 
   var rightMove = function(){
     var hasMoved=false;
-    for (i=1; i<=4; i++){
+    for (i=1; i<=5; i++){
       gridRow="grid_"+i;
-      $('#'+gridRow).children('#column4').attr('myAttr','cantMove');
-      while(($('#'+gridRow).children('#column1').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column2').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column3').attr('myAttr')=='canMove')){
-        for (j=3; j>=1; j--){
+      $('#'+gridRow).children('#column5').attr('myAttr','cantMove');
+      while(($('#'+gridRow).children('#column1').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column2').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column3').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column4').attr('myAttr')=='canMove')){
+        for (j=4; j>=1; j--){
           gridCol="column"+j;
           gridColPrev="column"+(j+1);
           if($('#'+gridRow).children('#'+gridCol).text()!="0"){
@@ -212,11 +232,11 @@ $(document).ready(function(){
 
   var upMove = function(){
     var hasMoved=false;
-    for (i=1; i<=4; i++){
+    for (i=1; i<=5; i++){
       gridCol="column"+i;
       $('#grid_1').children().attr('myAttr','cantMove');
-      while(($('#grid_2').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_3').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_4').children('#'+gridCol).attr('myAttr')=='canMove')){
-        for (j=2; j<=4; j++){
+      while(($('#grid_2').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_3').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_4').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_5').children('#'+gridCol).attr('myAttr')=='canMove')){
+        for (j=2; j<=5; j++){
           gridRow="grid_"+j;
           gridRowPrev="grid_"+(j-1);
           if($('#'+gridRow).children('#'+gridCol).text()!="0"){
@@ -254,11 +274,11 @@ $(document).ready(function(){
 
   var downMove = function(){
     var hasMoved=false;
-    for (i=1; i<=4; i++){
+    for (i=1; i<=5; i++){
       gridCol="column"+i;
-      $('#grid_4').children().attr('myAttr','cantMove');
-      while(($('#grid_2').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_3').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_1').children('#'+gridCol).attr('myAttr')=='canMove')){
-        for (j=3; j>=1; j--){
+      $('#grid_5').children().attr('myAttr','cantMove');
+      while(($('#grid_2').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_3').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_1').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_4').children('#'+gridCol).attr('myAttr')=='canMove')){
+        for (j=4; j>=1; j--){
           gridRow="grid_"+j;
           gridRowPrev="grid_"+(j+1);
           if($('#'+gridRow).children('#'+gridCol).text()!="0"){
@@ -294,11 +314,11 @@ $(document).ready(function(){
   }
 
   var checkWin = function(){
-    for(i=1; i<=4; i++){
+    for(i=1; i<=5; i++){
       var GridRow="grid_"+i;
-      for (j=1; j<=4; j++){
+      for (j=1; j<=5; j++){
         var GridCol="column"+j;
-        if ($('#'+GridRow).children('#'+GridCol).text()=='2048'){
+        if ($('#'+GridRow).children('#'+GridCol).text()=='65536'){
             winState="win";
             console.log("You win!")
         }     
@@ -311,9 +331,9 @@ $(document).ready(function(){
       var noZeroes=checkNoZeroes();
       if (noZeroes==true){
         var noSameValue=true;
-        for(i=1; i<=4; i++){
+        for(i=1; i<=5; i++){
           var GridRow="grid_"+i;
-          for (j=1; j<=3; j++){
+          for (j=1; j<=4; j++){
             var GridCol="column"+j;
             var GridColNext="column"+(j+1);
             if ($('#'+GridRow).children('#'+GridCol).text()==$('#'+GridRow).children('#'+GridColNext).text()){
@@ -321,9 +341,9 @@ $(document).ready(function(){
             }     
           }
         }
-        for(i=1; i<=4; i++){
+        for(i=1; i<=5; i++){
           var GridCol="column"+i;
-          for (j=1; j<=3; j++){
+          for (j=1; j<=4; j++){
             var GridRow="grid_"+j;
             var GridRowNext="grid_"+(j+1);
             if ($('#'+GridRow).children('#'+GridCol).text()==$('#'+GridRowNext).children('#'+GridCol).text()){
--- js/application6x6.js
+++ js/application6x6.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  var winState=null;
+  var over=null
   var totalScore=0;
 
   var printScore=function(){
@@ -9,9 +9,9 @@ $(document).ready(function(){
 
   var checkNoZeroes = function(){
     var hasZeroes = false;
-    for(i=1; i<=4; i++){
+    for(i=1; i<=6; i++){
       var GridRow="grid_"+i;
-      for (j=1; j<=4; j++){
+      for (j=1; j<=6; j++){
         var GridCol="column"+j;
         if ($('#'+GridRow).children('#'+GridCol).text()=='0'){
           hasZeroes=true;
@@ -22,9 +22,9 @@ $(document).ready(function(){
   }
 
   var reset = function(){
-    for(i=1; i<=4; i++){
+    for(i=1; i<=6; i++){
       var resetGridRow="grid_"+i;
-      for (j=1; j<=4; j++){
+      for (j=1; j<=6; j++){
         var resetGridCol="column"+j;
         $('#'+resetGridRow).children('#'+resetGridCol).attr('myAttr','canMove');
         $('#'+resetGridRow).children('#'+resetGridCol).attr('combine','canCombine');
@@ -33,13 +33,17 @@ $(document).ready(function(){
   }
 
   var setColor = function(){
-    for(i=1; i<=4; i++){
+    for(i=1; i<=6; i++){
       var GridRow="grid_"+i;
-      for (j=1; j<=4; j++){
+      for (j=1; j<=6; j++){
         var GridCol="column"+j;
         var elementValue=$('#'+GridRow).children('#'+GridCol);
         var elementValueText=elementValue.text()
         switch(elementValueText){
+          case "0":
+            elementValue.css('background-color','#CCC0B3');
+            elementValue.css('color','#CCC0B3');
+            break;
           case "2":
             elementValue.css('background-color','#EEE4DA');
             elementValue.css('color','#776e65');
@@ -84,9 +88,61 @@ $(document).ready(function(){
             elementValue.css('background-color','#EDC22E');
             elementValue.css('color','white');
             break;
+          case "4096":
+            elementValue.css('background-color','#FFA500');
+            elementValue.css('color','white');
+            break;
+          case "8192":
+            elementValue.css('background-color','#98F5FF');
+            elementValue.css('color','white');
+            break;
+          case "16384":
+            elementValue.css('background-color','#00C5CD');
+            elementValue.css('color','white');
+            break;
+          case "32768":
+            elementValue.css('background-color','#5CACEE');
+            elementValue.css('color','white');
+            break;
+          case "65536":
+            elementValue.css('background-color','#32CD32');
+            elementValue.css('color','white');
+            break;
+          case "131072":
+            elementValue.css('background-color','#00868B');
+            elementValue.css('color','white');
+            break;
+          case "262144":
+            elementValue.css('background-color','#EEAEEE');
+            elementValue.css('color','white');
+            break;
+          case "524288":
+            elementValue.css('background-color','#DC143C');
+            elementValue.css('color','white');
+            break;
+          case "1048576":
+            elementValue.css('background-color','#CAFF70');
+            elementValue.css('color','white');
+            break;
+          case "2097152":
+            elementValue.css('background-color','#68228B');
+            elementValue.css('color','white');
+            break;
+          case "4194304":
+            elementValue.css('background-color','#E9967A');
+            elementValue.css('color','white');
+            break;
+          case "8388608":
+            elementValue.css('background-color','#8E8E38');
+            elementValue.css('color','white');
+            break;
+          case "16777216":
+            elementValue.css('background-color','#8B1A1A');
+            elementValue.css('color','white');
+            break;
           default:
-            elementValue.css('background-color','#CCC0B3');
-            elementValue.css('color','#CCC0B3');
+            elementValue.css('background-color','#FFFF00');
+            elementValue.css('color','white');
         }
       }
     }
@@ -95,8 +151,8 @@ $(document).ready(function(){
   var randomNumberGeneration = function(){
     var generated=false;
     while (generated==false){
-      var colNum=Math.floor(Math.random()*3.99+1);
-      var rowNum=Math.floor(Math.random()*3.99+1);
+      var colNum=Math.floor(Math.random()*5.99+1);
+      var rowNum=Math.floor(Math.random()*5.99+1);
       gridCol="column"+colNum;
       gridRow="grid_"+rowNum;
       if($('#'+gridRow).children('#'+gridCol).text()=="0"){
@@ -110,11 +166,13 @@ $(document).ready(function(){
   printScore();
   randomNumberGeneration();
   randomNumberGeneration();
+  randomNumberGeneration();
+  randomNumberGeneration();
 
   $('.btn-default').click(function(){
-    for(i=1; i<=4; i++){
+    for(i=1; i<=6; i++){
       var resetGridRow="grid_"+i;
-      for (j=1; j<=4; j++){
+      for (j=1; j<=6; j++){
         var resetGridCol="column"+j;
         $('#'+resetGridRow).children('#'+resetGridCol).text('0');
         $('#'+resetGridRow).children('#'+resetGridCol).text('0');
@@ -122,19 +180,20 @@ $(document).ready(function(){
     }
     reset();
     totalScore=0;
-    winState=null;
     printScore();
+    randomNumberGeneration();
+    randomNumberGeneration();
     randomNumberGeneration();
     randomNumberGeneration();
   })
 
   var leftMove = function(){
   	var hasMoved=false;
-  	for (i=1; i<=4; i++){
+  	for (i=1; i<=6; i++){
   		gridRow="grid_"+i;
   		$('#'+gridRow).children('#column1').attr('myAttr','cantMove');
-  		while(($('#'+gridRow).children('#column2').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column3').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column4').attr('myAttr')=='canMove')){
-  			for (j=2; j<=4; j++){
+  		while(($('#'+gridRow).children('#column2').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column3').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column4').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column5').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column6').attr('myAttr')=='canMove')){
+  			for (j=2; j<=6; j++){
   				gridCol="column"+j;
   				gridColPrev="column"+(j-1);
   				if($('#'+gridRow).children('#'+gridCol).text()!="0"){
@@ -165,17 +224,20 @@ $(document).ready(function(){
   		}
   	}
   	reset();
-    if ((hasMoved==true)&&(winState==null)){randomNumberGeneration()};
+    if ((hasMoved==true)&&(over==null)){
+      randomNumberGeneration();
+      randomNumberGeneration();
+    }
     printScore();
   }
 
   var rightMove = function(){
     var hasMoved=false;
-    for (i=1; i<=4; i++){
+    for (i=1; i<=6; i++){
       gridRow="grid_"+i;
-      $('#'+gridRow).children('#column4').attr('myAttr','cantMove');
-      while(($('#'+gridRow).children('#column1').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column2').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column3').attr('myAttr')=='canMove')){
-        for (j=3; j>=1; j--){
+      $('#'+gridRow).children('#column6').attr('myAttr','cantMove');
+      while(($('#'+gridRow).children('#column1').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column2').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column3').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column4').attr('myAttr')=='canMove')||($('#'+gridRow).children('#column5').attr('myAttr')=='canMove')){
+        for (j=5; j>=1; j--){
           gridCol="column"+j;
           gridColPrev="column"+(j+1);
           if($('#'+gridRow).children('#'+gridCol).text()!="0"){
@@ -206,17 +268,20 @@ $(document).ready(function(){
       }
     }
     reset();
-    if ((hasMoved==true)&&(winState==null)){randomNumberGeneration()};
+    if ((hasMoved==true)&&(over==null)){
+      randomNumberGeneration();
+      randomNumberGeneration();
+    }    
     printScore();
   }
 
   var upMove = function(){
     var hasMoved=false;
-    for (i=1; i<=4; i++){
+    for (i=1; i<=6; i++){
       gridCol="column"+i;
       $('#grid_1').children().attr('myAttr','cantMove');
-      while(($('#grid_2').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_3').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_4').children('#'+gridCol).attr('myAttr')=='canMove')){
-        for (j=2; j<=4; j++){
+      while(($('#grid_2').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_3').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_4').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_5').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_6').children('#'+gridCol).attr('myAttr')=='canMove')){
+        for (j=2; j<=6; j++){
           gridRow="grid_"+j;
           gridRowPrev="grid_"+(j-1);
           if($('#'+gridRow).children('#'+gridCol).text()!="0"){
@@ -248,17 +313,20 @@ $(document).ready(function(){
       }
     }
     reset();
-    if ((hasMoved==true)&&(winState==null)){randomNumberGeneration()};
+    if ((hasMoved==true)&&(over==null)){
+      randomNumberGeneration();
+      randomNumberGeneration();
+    }    
     printScore();
   }
 
   var downMove = function(){
     var hasMoved=false;
-    for (i=1; i<=4; i++){
+    for (i=1; i<=6; i++){
       gridCol="column"+i;
-      $('#grid_4').children().attr('myAttr','cantMove');
-      while(($('#grid_2').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_3').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_1').children('#'+gridCol).attr('myAttr')=='canMove')){
-        for (j=3; j>=1; j--){
+      $('#grid_6').children().attr('myAttr','cantMove');
+      while(($('#grid_2').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_3').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_1').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_4').children('#'+gridCol).attr('myAttr')=='canMove')||($('#grid_5').children('#'+gridCol).attr('myAttr')=='canMove')){
+        for (j=5; j>=1; j--){
           gridRow="grid_"+j;
           gridRowPrev="grid_"+(j+1);
           if($('#'+gridRow).children('#'+gridCol).text()!="0"){
@@ -289,73 +357,57 @@ $(document).ready(function(){
       }
     }
     reset();
-    if ((hasMoved==true)&&(winState==null)){randomNumberGeneration()};
+    if ((hasMoved==true)&&(over==null)){
+      randomNumberGeneration();
+      randomNumberGeneration();
+    }    
     printScore();
   }
 
-  var checkWin = function(){
-    for(i=1; i<=4; i++){
-      var GridRow="grid_"+i;
-      for (j=1; j<=4; j++){
-        var GridCol="column"+j;
-        if ($('#'+GridRow).children('#'+GridCol).text()=='2048'){
-            winState="win";
-            console.log("You win!")
-        }     
+  var checkOver = function(){
+    var noZeroes=checkNoZeroes();
+    if (noZeroes==true){
+      var noSameValue=true;
+      for(i=1; i<=6; i++){
+        var GridRow="grid_"+i;
+        for (j=1; j<=5; j++){
+          var GridCol="column"+j;
+          var GridColNext="column"+(j+1);
+          if ($('#'+GridRow).children('#'+GridCol).text()==$('#'+GridRow).children('#'+GridColNext).text()){
+            noSameValue=false;
+          }     
+        }
+      }
+      for(i=1; i<=6; i++){
+        var GridCol="column"+i;
+        for (j=1; j<=5; j++){
+          var GridRow="grid_"+j;
+          var GridRowNext="grid_"+(j+1);
+          if ($('#'+GridRow).children('#'+GridCol).text()==$('#'+GridRowNext).children('#'+GridCol).text()){
+            noSameValue=false;
+          }     
+        }
       }
     }
-  }
-
-  var checkLose = function(){
-    if (winState!="win"){
-      var noZeroes=checkNoZeroes();
-      if (noZeroes==true){
-        var noSameValue=true;
-        for(i=1; i<=4; i++){
-          var GridRow="grid_"+i;
-          for (j=1; j<=3; j++){
-            var GridCol="column"+j;
-            var GridColNext="column"+(j+1);
-            if ($('#'+GridRow).children('#'+GridCol).text()==$('#'+GridRow).children('#'+GridColNext).text()){
-              noSameValue=false;
-            }     
-          }
-        }
-        for(i=1; i<=4; i++){
-          var GridCol="column"+i;
-          for (j=1; j<=3; j++){
-            var GridRow="grid_"+j;
-            var GridRowNext="grid_"+(j+1);
-            if ($('#'+GridRow).children('#'+GridCol).text()==$('#'+GridRowNext).children('#'+GridCol).text()){
-              noSameValue=false;
-            }     
-          }
-        }
-      }
-      if (noSameValue==true){
-        winState="lose";
-        console.log("You lose!")
-      }
-    }  
+    if (noSameValue==true){
+      over="over";
+      console.log("Game over! You got "+totalScore+" points! Good job!")
+    } 
   }
 
 	$("body").keydown(function(e) {
-	  if((e.keyCode == 37)&&(winState==null)) { // left
+	  if((e.keyCode == 37)&&(over==null)) { // left
 	    leftMove();
-      checkWin();
-      checkLose();
-	  } else if((e.keyCode == 39)&&(winState==null)) {
+      checkOver();
+	  } else if((e.keyCode == 39)&&(over==null)) {
       rightMove();
-      checkWin();
-      checkLose();
-    } else if((e.keyCode == 38)&&(winState==null)){
+      checkOver();
+    } else if((e.keyCode == 38)&&(over==null)){
       upMove();
-      checkWin();
-      checkLose();
-    } else if((e.keyCode == 40)&&(winState==null)){
+      checkOver();
+    } else if((e.keyCode == 40)&&(over==null)){
       downMove();
-      checkWin();
-      checkLose();
+      checkOver();
     }
 	});
 
